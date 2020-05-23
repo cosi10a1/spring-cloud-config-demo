fc5b6b1c6b7d303d940578f419f6d9f32e929bd3
Dennis Winter <dennis.winter@alphabrik.com>
2020-05-23 13:19:47 +0200

    fixing yml issue with failed indentation

--- app/src/main/kotlin/com/alphabrik/inditex/cloud/config/app/AppApplication.kt
+++ app/src/main/kotlin/com/alphabrik/inditex/cloud/config/app/AppApplication.kt
@@ -1,5 +1,6 @@
 package com.alphabrik.inditex.cloud.config.app
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.context.config.annotation.RefreshScope
@@ -7,7 +8,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope
 @SpringBootApplication
 @RefreshScope
 class AppApplication
-
+@Value("\${configuration.projectName}")
+lateinit var test: String
 fun main(args: Array<String>) {
     runApplication<AppApplication>(*args)
+    println(test)
 }
